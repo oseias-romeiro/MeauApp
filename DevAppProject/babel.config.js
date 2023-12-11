4544aa5658b91ca61a7c167ffed7a305b566c07b
oseias-romeiro <oseiasromeirom@gmail.com>
2023-12-11 16:47:19 -0300

    fix drawer

--- DevAppProject/babel.config.js
+++ DevAppProject/babel.config.js
@@ -1,7 +1,16 @@
 module.exports = function(api) {
-  api.cache(false);
+  api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["module:react-native-dotenv", 'react-native-reanimated/plugin'],
+    plugins: [
+      "module:react-native-dotenv",
+      [
+        "module-resolver",
+        {
+          extensions: [".tsx", ".ts", ".js", ".json"],
+        },
+      ],
+      'react-native-reanimated/plugin'
+    ],
   };
 };
